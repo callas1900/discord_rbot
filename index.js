c42adda5bddeb0810f3b8962aa59ba6f30c6df22
Ryo Tanaka <ryo.tanaka@yamaneco.io>
2021-07-17 16:04:39 +0900

    refactoring

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('message', async message => {
     try {
         const ret = await cmd.exec(message)
         if (ret.msg) { message.channel.send(ret.msg.message, ret.msg.component) }
-        if (ret.timers) { timerutil(ret.timers, cmd.id, taskutil(message)) }
+        if (ret.timers) { timerutil(ret.timers, cmd.id, taskFactory(message, cmd.id)) }
     }
     catch (error) {
         console.error(error)
